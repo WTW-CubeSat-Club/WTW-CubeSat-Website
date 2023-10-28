bcab1a11e57cc27181b935096c4226390c4627ab
William65536 <110579495+William65536@users.noreply.github.com>
2023-10-28 12:58:15 -0400

    Updated icons.

--- script.js
+++ script.js
@@ -1,38 +1,55 @@
 // TODO: Refactor in TypeScript
 
-const logo = document.querySelector('img#logo');
-const instaLogo = document.querySelector('img#insta-logo');
-const twitterLogo = document.querySelector('img#twitter-logo');
+// Lightness Mode
+{
+    const icons = new Map([
+        [document.querySelector('img#logo'), 'CubeSatIcon'],
+        [document.querySelector('img#insta-icon'), 'InstagramIcon'],
+        [document.querySelector('img#twitter-icon'), 'TwitterIcon'],
+        [document.querySelector('img#github-icon'), 'GithubIcon'],
+        [document.querySelector('img#school-icon'), 'SchoolIcon'],
+        [document.querySelector('img#mail-icon'), 'MailIcon'],
+        [document.querySelector('img#rocket-icon'), 'RocketTopIcon'],
+    ]);
 
-const swapStyle = isLight => {
-    logo.src = isLight ? 'assets/CubeSatLogoInverse.svg' : 'assets/CubeSatLogoWhite.svg';
-    instaLogo.src = isLight ? 'assets/InstagramLogo.svg' : 'assets/InstagramLogoWhite.svg';
-    twitterLogo.src = isLight ? 'assets/TwitterLogo.svg' : 'assets/TwitterLogoWhite.svg';
+    const swapStyle = isLight => {
+        for (const [key, value] of icons) {
+            key.src = `assets/${value + (isLight ? '' : 'White')}.svg`;
+        }
 
-    document.querySelector('link#mystylesheet').href = isLight ? '' : 'darkmode.css';
-    localStorage.setItem('theme', isLight);
-};
+        document.querySelector('link#mystylesheet').href = isLight ? '' : 'darkmode.css'; // TODO: Maybe the node should be deleted instead
+        localStorage.setItem('theme', isLight);
+    };
 
-const isLight = localStorage.getItem('theme') == 'true';
+    const isLight = localStorage.getItem('theme') == 'true';
 
-swapStyle(isLight);
+    swapStyle(isLight);
 
-const lightSwitch = document.querySelector('input#light-switch');
-lightSwitch.addEventListener('change', () => swapStyle(!lightSwitch.checked));
+    const lightSwitch = document.querySelector('input#light-switch');
+    lightSwitch.addEventListener('change', () => swapStyle(!lightSwitch.checked));
 
-if (!isLight) {
-    lightSwitch.checked = true;
+    if (!isLight) {
+        lightSwitch.checked = true;
+    }
 }
 
-// Get the button
-const scrollButton = document.querySelector('button#scroll-button');
-
-// When the user scrolls down 500px from the top of the document, show the button
-window.onscroll =
-    () => scrollButton.style.display = document.body.scrollTop > 1_500 || document.documentElement.scrollTop > 1_500
-        ? 'block'
-        : 'none';
+// Scroll button
 
 // When the user clicks on the button, scroll to the top of the document
 const scrollToTop =
     () => document.body.scrollTop = document.documentElement.scrollTop = 0;
+
+{
+    const scrollButton = document.querySelector('button#scroll-button');
+
+    // When the user scrolls down 500px from the top of the document, show the button
+
+    const showScrollButton =
+        () => scrollButton.style.display = document.body.scrollTop > 1_500 || document.documentElement.scrollTop > 1_500
+            ? 'block'
+            : 'none';
+
+    showScrollButton();
+
+    window.addEventListener('scroll', showScrollButton);
+}
